a7f040db03e46cab77e7141c283fcf3245d10051
Charlie Szymanski <charlie.is@duke.edu>
2012-01-23 16:24:40 -0500

    gzip fix

--- gzip_assets.py
+++ gzip_assets.py
@@ -20,7 +20,7 @@ shutil.copytree(os.path.join(project_dir, 'assets'), os.path.join(project_dir, '
 for path, dirs, files in os.walk(os.path.join(project_dir, 'gzip/assets')):
     for filename in files:
         if filename[-3:] in ['gif', 'png', 'jpg']:
-            continue
+            pass
     
         file_path = os.path.join(path, filename)
         
